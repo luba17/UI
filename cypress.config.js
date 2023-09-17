c60985e0f0393182db941fd7f95bbdeb44486669
luba17 <lm@crosslife.me>
2023-09-17 11:51:28 +0300

    added projictID for dashboard and added command for running cypress. Task 15.1

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+    projectId: "ismq7t",
+    // ...rest of the Cypress project config,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
